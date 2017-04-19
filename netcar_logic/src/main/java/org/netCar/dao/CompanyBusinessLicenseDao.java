fafd9dfa3a4be53d7d38ce91e3b1f96fb0e272d5
xin.men <menxin_job@163.com>
2017-04-19 15:36:52 +0800

    公司营业证信息修改

--- netcar_logic/src/main/java/org/netCar/dao/CompanyBusinessLicenseDao.java
+++ netcar_logic/src/main/java/org/netCar/dao/CompanyBusinessLicenseDao.java
@@ -1,11 +1,11 @@
 package org.netCar.dao;
 
-import org.netCar.domain.CompanyBusinessLicenseEntity;
+import org.netCar.domain.CompanyPermitEntity;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by gujgkjg on 2017/4/19.
  */
 @Repository
-public class CompanyBusinessLicenseDao extends BaseHBDao<CompanyBusinessLicenseEntity, Integer> {
+public class CompanyBusinessLicenseDao extends BaseHBDao<CompanyPermitEntity, Integer> {
 }
